ebd6b23c464d4825e2832e54721d49b8b3081aac
Julien BONACHERA <jbonachera@xebia.fr>
2018-06-20 23:46:09 +0200

    dns: add lb IP to wilcard domain

--- cloudflare.tf
+++ cloudflare.tf
@@ -41,3 +41,12 @@ resource "cloudflare_record" "nomad_agent" {
   type   = "A"
   ttl    = 1
 }
+
+resource "cloudflare_record" "entrypoint" {
+  count  = "${var.lb_count}"
+  domain = "${var.cloudflare_domain}"
+  name   = "cloud"
+  value  = "${element(scaleway_server.nomad-lb.*.public_ip, count.index)}"
+  type   = "A"
+  ttl    = 1
+}
