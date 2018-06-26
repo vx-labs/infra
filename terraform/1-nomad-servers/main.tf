ff03ac6fc4ea6829596560d16c8b06b4aee9675d
Julien BONACHERA <jbonachera@xebia.fr>
2018-06-26 18:00:02 +0200

    server: wait for cluster to be ready when destroying

--- terraform/1-nomad-servers/main.tf
+++ terraform/1-nomad-servers/main.tf
@@ -3,6 +3,9 @@ data "scaleway_image" "server" {
   name         = "coreos-nomad-server"
 }
 resource "scaleway_server" "nomad-server" {
+  lifecycle {
+    create_before_destroy = true
+  }
   name  = "nomad-server"
   image = "${data.scaleway_image.server.id}"
   dynamic_ip_required = true
@@ -11,6 +14,17 @@ resource "scaleway_server" "nomad-server" {
   count = "${var.master_count}"
   boot_type = "local"
   security_group = "${scaleway_security_group.nomad_server.id}"
+  tags  = [
+    "CLUSTER_SIZE=${var.master_count}",
+  ]
+  provisioner "local-exec" {
+    command = "docker run --rm quay.io/vxlabs/nomad-remove-local-peer server"
+    when = "destroy"
+    environment = {
+      NOMAD_ADDR="http://${self.public_ip}:4646"
+      CONSUL_HTTP_ADDR="${self.public_ip}:8500"
+    }
+  }
 }
 
 resource "scaleway_security_group" "nomad_server" {
