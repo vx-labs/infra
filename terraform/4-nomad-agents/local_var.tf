5ebb12b1db4af3db13793274f5a2138031b2d48f
Julien BONACHERA <jbonachera@xebia.fr>
2018-06-30 23:32:47 +0200

    nomad-agent: upgrade image

--- terraform/4-nomad-agents/local_var.tf
+++ terraform/4-nomad-agents/local_var.tf
@@ -1,7 +1,7 @@
 variable "agent_images" {
   default = [
-    "coreos-nomad-agent-5b37d4b4-6501-b24c-4f5c-371388448e39",
-    "coreos-nomad-agent-5b37d4b4-6501-b24c-4f5c-371388448e39",
-    "coreos-nomad-agent-5b37d4b4-6501-b24c-4f5c-371388448e39"
+    "coreos-nomad-agent-5b37e03c-1a4c-6d81-4454-e3623407e405",
+    "coreos-nomad-agent-5b37e03c-1a4c-6d81-4454-e3623407e405",
+    "coreos-nomad-agent-5b37e03c-1a4c-6d81-4454-e3623407e405"
   ]
 }
