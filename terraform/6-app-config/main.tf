0855235147efae8047a0a171f37543d2b54c2275
Julien BONACHERA <jbonachera@xebia.fr>
2018-07-20 12:47:53 +0200

    terramform: add app_config

---
+++ terraform/6-app-config/main.tf
@@ -0,0 +1,29 @@
+provider "vault" {}
+provider "consul" {
+}
+
+variable "letsencrypt_email" {}
+variable "mqtt_auth_tokens" {}
+
+resource "consul_key_prefix" "mqtt_config" {
+  path_prefix = "mqtt/conf/"
+
+  subkeys = {
+    "tls"     = <<EOT
+{
+"cn": "broker.iot.cloud.${var.cloudflare_domain}",
+"le_email": "${var.letsencrypt_email}"
+}
+EOT
+  }
+}
+
+resource "vault_generic_secret" "mqtt-config" {
+  path      = "/secret/data/mqtt/authentication"
+  data_json = <<EOT
+{
+  "static_tokens": ["${join("\",\"", split(",", var.mqtt_auth_tokens))}"]
+}
+EOT
+}
+
