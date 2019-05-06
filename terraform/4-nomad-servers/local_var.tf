87d161100d5a753e1910c46ed69f5553843b579f
Julien BONACHERA <julien.bonachera@fortuneo.com>
2019-05-06 18:15:48 +0200

    migrate masters to new module

--- terraform/4-nomad-servers/local_var.tf
+++ terraform/4-nomad-servers/local_var.tf
@@ -1,7 +1,7 @@
 variable "master_images" {
   default = [
-    "master-5ccdfa2c-44ca-5ee9-110b-bd76d57277a2",
-    "master-5ccdfa2c-44ca-5ee9-110b-bd76d57277a2",
+    "coreos-5ccffd06-ad32-8dff-9ed6-2a2edc3eed3a",
+    "coreos-5ccffd06-ad32-8dff-9ed6-2a2edc3eed3a",
     "master-5ccdfa2c-44ca-5ee9-110b-bd76d57277a2",
   ]
 }
--- terraform/4-nomad-servers/main.tf
+++ terraform/4-nomad-servers/main.tf
@@ -1,38 +1,64 @@
 provider "vault" {}
 
 module "server-1" {
-  source       = "./modules/nomad-master"
-  image        = "${element(var.master_images, 0)}"
-  secgroup     = "${scaleway_security_group.nomad_server.id}"
-  index        = "1"
-  expect_count = "${length(var.master_images)}"
-  region       = "${var.region}"
-  domain       = "${var.cloudflare_domain}"
-  public_ip    = false
+  source           = "../modules/instance"
+  image            = "${element(var.master_images, 0)}"
+  secgroup         = "${scaleway_security_group.nomad_server.id}"
+  hostname         = "nomad-server-1"
+  region           = "${var.region}"
+  domain           = "${var.cloudflare_domain}"
+  cloudinit        = "${file("config.yaml")}"
+  discovery_record = "servers.nomad"
+  vault_role       = "nomad-role"
+  vault_token_role = "nomad-cluster"
+  user_data_count  = 1
+}
+
+resource "scaleway_user_data" "cluster_size_1" {
+  server = "${module.server-1.instance_id}"
+  key    = "CLUSTER_SIZE"
+  value  = "${length(var.master_images)}"
 }
 
 module "server-2" {
-  source       = "./modules/nomad-master"
-  image        = "${element(var.master_images, 1)}"
-  secgroup     = "${scaleway_security_group.nomad_server.id}"
-  index        = "2"
-  expect_count = "${length(var.master_images)}"
-  region       = "${var.region}"
-  domain       = "${var.cloudflare_domain}"
-  public_ip    = false
+  source           = "../modules/instance"
+  image            = "${element(var.master_images, 1)}"
+  secgroup         = "${scaleway_security_group.nomad_server.id}"
+  hostname         = "nomad-server-2"
+  region           = "${var.region}"
+  domain           = "${var.cloudflare_domain}"
+  cloudinit        = "${file("config.yaml")}"
+  discovery_record = "servers.nomad"
+  vault_role       = "nomad-role"
+  vault_token_role = "nomad-cluster"
+  user_data_count  = 1
+}
+
+resource "scaleway_user_data" "cluster_size_2" {
+  server = "${module.server-2.instance_id}"
+  key    = "CLUSTER_SIZE"
+  value  = "${length(var.master_images)}"
 }
 
 module "server-3" {
-  source       = "./modules/nomad-master"
-  image        = "${element(var.master_images, 2)}"
-  secgroup     = "${scaleway_security_group.nomad_server.id}"
-  index        = "3"
-  expect_count = "${length(var.master_images)}"
-  region       = "${var.region}"
-  domain       = "${var.cloudflare_domain}"
-  public_ip    = false
+  source           = "../modules/instance"
+  image            = "${element(var.master_images, 2)}"
+  secgroup         = "${scaleway_security_group.nomad_server.id}"
+  hostname         = "nomad-server-3"
+  region           = "${var.region}"
+  domain           = "${var.cloudflare_domain}"
+  cloudinit        = "${file("config.yaml")}"
+  discovery_record = "servers.nomad"
+  vault_role       = "nomad-role"
+  vault_token_role = "nomad-cluster"
+  user_data_count  = 1
 }
 
+resource "scaleway_user_data" "cluster_size_3" {
+  server = "${module.server-3.instance_id}"
+  key    = "CLUSTER_SIZE"
+  value  = "${length(var.master_images)}"
+}
 resource "scaleway_security_group" "nomad_server" {
   name        = "nomad-server"
   description = "Nomad servers (masters)"
