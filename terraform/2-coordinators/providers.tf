a8b06a28e50f8379220644cc24175ab6b38e3767
Julien BONACHERA <julien.bonachera@fortuneo.com>
2019-12-15 18:11:15 +0100

    upgrade coordinators

--- terraform/2-coordinators/providers.tf
+++ terraform/2-coordinators/providers.tf
@@ -5,14 +5,10 @@ variable "cloudflare_token" {}
 variable "cloudflare_domain" {}
 
 provider "cloudflare" {
-  version = "~> 1.0"
+  version = "~> 2.0"
   email   = var.cloudflare_email
-  token   = var.cloudflare_token
+  api_key = var.cloudflare_token
 }
-
-variable "scw_api_organization" {}
-variable "scw_access_key" {}
-variable "scw_secret_key" {}
 
 variable "region" {
   default = "fr-par"
@@ -24,9 +20,6 @@ variable "zone" {
 
 provider "scaleway" {
   version      = "~> 1.11"
-  organization = var.scw_api_organization
-  access_key   = var.scw_access_key
-  secret_key   = var.scw_secret_key
   region       = var.region
   zone         = var.zone
 }
