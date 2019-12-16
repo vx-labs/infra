489e58a9ea88b37b6a6d9a91a97846b9b9289efb
Julien BONACHERA <julien.bonachera@fortuneo.com>
2019-12-16 19:20:39 +0100

    upgrade coordinators

--- terraform/2-coordinators/main.tf
+++ terraform/2-coordinators/main.tf
@@ -1,12 +1,18 @@
+resource "scaleway_instance_placement_group" "availability_group" {
+  name        = "coordinators"
+  policy_mode = "enforced"
+}
+
 module "coordinator-1" {
-  source           = "../modules/instance-v2"
-  image            = element(var.coordinator_images, 0)
-  secgroup         = scaleway_security_group.coordinator.id
-  hostname         = "coordinator-1"
-  region           = var.region
-  domain           = var.cloudflare_domain
-  cloud_init       = file("config.yaml")
-  discovery_record = "servers.consul"
+  source             = "../modules/instance-v2"
+  image              = element(var.coordinator_images, 0)
+  secgroup           = scaleway_security_group.coordinator.id
+  hostname           = "coordinator-1"
+  region             = var.region
+  domain             = var.cloudflare_domain
+  cloud_init         = file("config.yaml")
+  placement_group_id = scaleway_instance_placement_group.availability_group.id
+  discovery_record   = "servers.consul"
   user_data = [{
     key   = "CONSUL_JOIN_LIST"
     value = "servers.consul.discovery.${var.region}.${var.cloudflare_domain}"
@@ -17,14 +23,15 @@ module "coordinator-1" {
 }
 
 module "coordinator-2" {
-  source           = "../modules/instance-v2"
-  image            = element(var.coordinator_images, 1)
-  secgroup         = scaleway_security_group.coordinator.id
-  hostname         = "coordinator-2"
-  region           = var.region
-  domain           = var.cloudflare_domain
-  cloud_init       = file("config.yaml")
-  discovery_record = "servers.consul"
+  source             = "../modules/instance-v2"
+  image              = element(var.coordinator_images, 1)
+  secgroup           = scaleway_security_group.coordinator.id
+  hostname           = "coordinator-2"
+  region             = var.region
+  domain             = var.cloudflare_domain
+  cloud_init         = file("config.yaml")
+  placement_group_id = scaleway_instance_placement_group.availability_group.id
+  discovery_record   = "servers.consul"
   user_data = [{
     key   = "CONSUL_JOIN_LIST"
     value = "servers.consul.discovery.${var.region}.${var.cloudflare_domain}"
@@ -35,14 +42,15 @@ module "coordinator-2" {
 }
 
 module "coordinator-3" {
-  source           = "../modules/instance-v2"
-  image            = element(var.coordinator_images, 2)
-  secgroup         = scaleway_security_group.coordinator.id
-  hostname         = "coordinator-3"
-  region           = var.region
-  domain           = var.cloudflare_domain
-  cloud_init       = file("config.yaml")
-  discovery_record = "servers.consul"
+  source             = "../modules/instance-v2"
+  image              = element(var.coordinator_images, 2)
+  secgroup           = scaleway_security_group.coordinator.id
+  hostname           = "coordinator-3"
+  region             = var.region
+  domain             = var.cloudflare_domain
+  cloud_init         = file("config.yaml")
+  placement_group_id = scaleway_instance_placement_group.availability_group.id
+  discovery_record   = "servers.consul"
   user_data = [{
     key   = "CONSUL_JOIN_LIST"
     value = "servers.consul.discovery.${var.region}.${var.cloudflare_domain}"
